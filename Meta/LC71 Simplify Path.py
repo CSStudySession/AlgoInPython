2940146d2f6b6c13be4ff6dc195fa7266989585a
aaron.liu <992362427@qq.com>
2025-05-15 12:43:26 -0700

    update

--- Meta/LC71 Simplify Path.py
+++ Meta/LC71 Simplify Path.py
@@ -1,41 +1,52 @@
 '''
 Meta version
-Given current directory and change directory path, return final path.
-
+Given current directory cwd already simlified, 
+and change directory path cd, return final path.
 For Example:
 Curent                 Change            Output
 
 /                    /facebook           /facebook
 /facebook/anin       ../abc/def          /facebook/abc/def
 /facebook/instagram   ../../../../.      /
+/fb/a                empty               /fb/a
+/a/c/d               /t/e/.././f            /t/f
+思路:stack 用stack存cwd用/split之后的子结果 注意1.不存空 2.cd以/开头时 cwd先清空
+再遍历cd用/ split的子结果 根据不用情况选择pop stack, continue, append stack.
+最后返回/ + /.join(stack)
+T(n) S(n)
 '''
-
-def change_path(cwd: str, cd: str) -> str:
-    path = cwd + "/" + cd  # 先把两个path用'/'连起来
-    stack = []
-    for dir in path.split('/'):
-        if dir == ".":
+def change_directory(cwd: str, cd: str) -> str:
+    if not cd:
+        return cwd
+    if cd[0] == '/':
+        cwd = ''
+    dirs = []
+    for dir in cwd.split('/'):
+        if dir: # 可能有'///a/'这种多个/相邻情况 多个/认为是一个 split后中间是空字符 忽略
+            dirs.append(dir)
+    
+    for dir in cd.split('/'):
+        if not dir:
             continue
-        elif dir == "..":
-            if stack: 
-                stack.pop()
-        elif not dir:  # 注意 如果当前dir为空 题意需要回到根目录 
-            stack.clear()
+        if dir == '.':
+            continue
+        elif dir == '..':
+            if dirs:
+                dirs.pop()
         else:
-            stack.append(dir)
-
-    return "/" + "/".join(stack)
+            dirs.append(dir)    
+    return '/' + '/'.join(dirs)
 
 # unit test
 cwd1 = '/facebook/anin'
 cd1 = '../abc/def'
-print(change_path(cwd1, cd1))
-cwd2 = '/'
-cd2 = '/facebook'
-print(change_path(cwd2, cd2))
-cwd3 = '/facebook/anin'
-cd3 = '..//def'
-print(change_path(cwd3, cd3))
+print(change_directory(cwd1, cd1))
+cwd2 = '/a/bb/c/d'
+cd2 = '/facebook/./q/we'
+print(change_directory(cwd2, cd2))
+cwd3 = ''
+cd3 = '/a/../c'
+print(change_directory(cwd3, cd3))
 
 '''
 用stack存最终返回path里面的子path
