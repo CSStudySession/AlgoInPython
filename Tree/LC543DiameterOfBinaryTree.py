6d2e3dc6848e181940ad88dac0371ee939d7cab7
aaron.liu <zlsuperdan@gmail.com>
2025-06-05 22:46:28 -0700

    update

--- Tree/LC543DiameterOfBinaryTree.py
+++ Tree/LC543DiameterOfBinaryTree.py
@@ -1,6 +1,7 @@
 from typing import Optional
 '''
-The diameter of a binary tree is the length of the longest path between any two nodes in a tree. 
+The diameter of a binary tree is the length of the longest path 
+between any two nodes in a tree. 
 This path may or may not pass through the root.
 '''
 # Definition for a binary tree node.
@@ -27,7 +28,69 @@ def maxHeight(root, ret) -> int:
     return max(left_ret, right_ret) + 1 # 这里要+1.左右取较大的 加上自己返回
 
 '''
-followup: return longest diameter path
+variant1: given a N-ary tree and return the diameter.
+思路: dfs with backtrack. 对node_x 设有3个chilren:
+        node_x
+     c1   c2   c3
+循环遍历c1-->c3 求得每个c的最长path 记录top 2 length of path: max, second
+则当前见过的最长diameter=max+second,node_x返回给上层max+1(包含自己这条边)
+注意如何在递归中存diameter:用一个list ret, 可以动态更新ret[0]
+T(n) S(n)
+'''
+def diameter_n_ary_tree(root: Optional[TreeNode]) -> int:
+    if not TreeNode:
+        return 0
+    ret = []
+    get_diameter(root, ret)
+    return ret[0]
+
+def get_diameter(node: Optional[TreeNode], ret:list[int]) -> int:
+    if not node:
+        return 0
+    max_len, second_len = 0, 0
+    for child in node.chilren:
+        cur_len = get_diameter(child, ret)
+        if cur_len > max_len:
+            second_len, max_len = max_len, cur_len
+        elif cur_len > second_len:
+            second_len = cur_len
+    ret[0] = max(ret[0], max_len + second_len)
+    return max_len + 1
+
+'''
+variant2: find the diameter, but all the nodes on the diameter path need to have 
+the same value. e.g.:
+          3a
+    3b         3d
+ 1    3c     4    5
+diameter is:3, formed by: 3c->3d->3a->3d 值全都得一样才行 
+思路:同原题思路 变化是 在dfs处理当前node时 加个判断:自己的左右孩子与自己的val一样 才能延伸路径
+不一样的话 return 0
+T(n) S(h) worst n
+'''
+def longestUnivaluePath(root: TreeNode) -> int:
+    ret = [0]
+    def dfs(node, ret):
+        if not node:
+            return 0
+        # 后序遍历
+        left = dfs(node.left, ret)
+        right = dfs(node.right, ret)
+        # 如果子节点存在且值相同，则延伸路径
+        left_path = right_path = 0
+        if node.left and node.left.val == node.val:
+            left_path = left + 1
+        if node.right and node.right.val == node.val:
+            right_path = right + 1
+        # 更新全局最大路径
+        ret[0] = max(ret[0], left_path + right_path)
+        # 返回给父节点的最长单边路径
+        return max(left_path, right_path) # 更新l/r_path时 只有与左/右值一样才能+1 否则就是0
+    dfs(root, ret)
+    return ret[0]
+
+'''
+variant3: same setup as OG but return longest diameter path
 思路: 使用DFS后序遍历 在每个节点处计算:
 - 当前节点左子树的最大高度 left_height
 - 当前节点右子树的最大高度 right_height
@@ -61,33 +124,3 @@ def dfs(node: Optional[TreeNode], diameter, best_path_holder) -> tuple[int, list
         return left_height + 1, left_path + [node.val]
     else:
         return right_height + 1, right_path + [node.val]
-
-'''
-variant: given a N-ary tree and return the diameter.
-思路: dfs with backtrack. 对node_x 设有3个chilren:
-        node_x
-     c1   c2   c3
-循环遍历c1-->c3 求得每个c的最长path 记录top 2 length of path: max, second
-则当前见过的最长diameter=max+second,node_x返回给上层max+1(包含自己这条边)
-注意如何在递归中存diameter:用一个list ret, 可以动态更新ret[0]
-T(n) S(n)
-'''
-def diameter_n_ary_tree(root: Optional[TreeNode]) -> int:
-    if not TreeNode:
-        return 0
-    ret = []
-    get_diameter(root, ret)
-    return ret[0]
-
-def get_diameter(node: Optional[TreeNode], ret:list[int]) -> int:
-    if not node:
-        return 0
-    max_len, second_len = 0, 0
-    for child in node.chilren:
-        cur_len = get_diameter(child, ret)
-        if cur_len > max_len:
-            second_len, max_len = max_len, cur_len
-        elif cur_len > second_len:
-            second_len = cur_len
-    ret[0] = max(ret[0], max_len + second_len)
-    return max_len + 1
